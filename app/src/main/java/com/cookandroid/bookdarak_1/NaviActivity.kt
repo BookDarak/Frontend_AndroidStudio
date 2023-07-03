7ef194aa3fe65956208fa013248d5d5a45ee2f65
minminblue <87056558+minminblue@users.noreply.github.com>
2023-07-03 23:18:22 +0900

    Navigationbar

---
+++ app/src/main/java/com/cookandroid/bookdarak_1/NaviActivity.kt
@@ -0,0 +1,104 @@
+package com.cookandroid.bookdarak_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.cookandroid.bookdarak_1.databinding.ActivityNaviBinding
+
+
+
+private const val TAG_CALENDER = "calender_fragment"
+private const val TAG_HOME = "home_fragment"
+private const val TAG_MY_PAGE = "my_page_fragment"
+private const val TAG_FIND = "find_fragment"
+private const val TAG_REVIEW = "review_fragment"
+
+class NaviActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityNaviBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNaviBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        setFragment(TAG_HOME, HomeFragment())
+
+        binding.navigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.calenderFragment -> setFragment(TAG_CALENDER, CalendarFragment())
+                R.id.homeFragment -> setFragment(TAG_HOME, HomeFragment())
+                R.id.myPageFragment-> setFragment(TAG_MY_PAGE, MyPageFragment())
+                R.id.reviewFragment -> setFragment(TAG_REVIEW, ReviewFragment())
+                R.id.findFragment -> setFragment(TAG_FIND, FindFragment())
+            }
+            true
+        }
+    }
+
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val fragTransaction = manager.beginTransaction()
+
+        if (manager.findFragmentByTag(tag) == null){
+            fragTransaction.add(R.id.mainFrameLayout, fragment, tag)
+        }
+
+        val calender = manager.findFragmentByTag(TAG_CALENDER)
+        val home = manager.findFragmentByTag(TAG_HOME)
+        val myPage = manager.findFragmentByTag(TAG_MY_PAGE)
+        val find = manager.findFragmentByTag(TAG_FIND)
+        val review = manager.findFragmentByTag(TAG_REVIEW)
+
+
+        if (calender != null){
+            fragTransaction.hide(calender)
+        }
+
+        if (home != null){
+            fragTransaction.hide(home)
+        }
+
+        if (myPage != null) {
+            fragTransaction.hide(myPage)
+        }
+        if (find != null) {
+            fragTransaction.hide(find)
+        }
+        if (review != null) {
+            fragTransaction.hide(review)
+        }
+
+        if (tag == TAG_CALENDER) {
+            if (calender!=null){
+                fragTransaction.show(calender)
+            }
+        }
+        else if (tag == TAG_HOME) {
+            if (home != null) {
+                fragTransaction.show(home)
+            }
+        }
+
+        else if (tag == TAG_MY_PAGE){
+            if (myPage != null){
+                fragTransaction.show(myPage)
+            }
+        }
+
+        else if (tag == TAG_FIND){
+            if (find != null){
+                fragTransaction.show(find)
+            }
+        }
+        else if (tag == TAG_REVIEW){
+            if (review != null){
+                fragTransaction.show(review)
+            }
+        }
+
+        fragTransaction.commitAllowingStateLoss()
+    }
+}
