800c0dde37a19f06272b1a0689fec5bf1fd669b0
minminblue <87056558+minminblue@users.noreply.github.com>
2023-07-05 14:46:54 +0900

    bookinfo_1

---
+++ app/src/main/java/com/cookandroid/bookdarak_1/BookinfoActivity.kt
@@ -0,0 +1,17 @@
+package com.cookandroid.bookdarak_1
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class BookinfoActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookinfo)
+
+
+    }
+
+}
