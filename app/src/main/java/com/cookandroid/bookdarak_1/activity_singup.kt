ee50e0a2c545be96f75c05f2360e1959c5eeb3d7
bigwaveBigwave <gb6ke3@naver.com>
2023-07-08 20:12:47 +0900

    add MainActivity.kt - login code revise bookinfo.xml - button, textcolor

--- app/src/main/java/com/cookandroid/bookdarak_1/activity_singup.kt
+++ app/src/main/java/com/cookandroid/bookdarak_1/activity_singup.kt
@@ -1,11 +1,20 @@
 package com.cookandroid.bookdarak_1
 
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+
 
 class activity_singup : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_singup)
+
+
     }
 }
