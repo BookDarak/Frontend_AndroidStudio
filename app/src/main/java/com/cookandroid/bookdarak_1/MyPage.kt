8c65c6c83e262d3e7cea4b4536605106d7c03d78
bigwaveBigwave <gb6ke3@naver.com>
2023-07-19 23:36:01 +0900

    feat : Add activity_my_page.xml

---
+++ app/src/main/java/com/cookandroid/bookdarak_1/MyPage.kt
@@ -0,0 +1,11 @@
+package com.cookandroid.bookdarak_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
