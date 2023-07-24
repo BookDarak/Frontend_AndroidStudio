6fd230b93e8202df531a874d8fe70a5fc6f23f0f
bigwaveBigwave <gb6ke3@naver.com>
2023-07-24 23:44:00 +0900

    feat : userpage.xml

---
+++ app/src/main/java/com/cookandroid/bookdarak_1/Userpage.kt
@@ -0,0 +1,11 @@
+package com.cookandroid.bookdarak_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Userpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_userpage)
+    }
+}
